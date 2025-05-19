2dfc71aa051db4e7c9a7e86b92d304415890f9d2
GABRIEL JOSÉ SANTOS DA SILVA <25132745@dsn127m1242374.cfp127aluno.local>
2025-05-19 08:42:30 -0300

    link do menu

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const btnAdd = document.querySelectorAll('.add-produto');
 
-
 btnAdd.forEach(botao => {
     botao.addEventListener('click', function() {
-        botao.classList.add('jello-horizontal');
-        botao.classList.add('add-produto-inverso');
-        botao.textContent = "-";
-        setTimeout(() => {
-            botao.classList.remove('jello-horizontal');
-          }, 1000);
-    })
-})
+        if (botao.classList.contains('add-produto-inverso')) {
+            botao.classList.remove('add-produto-inverso');
+            botao.textContent = "+";
+        } else {
+            botao.classList.add('jello-horizontal');
+            botao.classList.add('add-produto-inverso');
+            botao.textContent = "-";
+            setTimeout(() => {
+                botao.classList.remove('jello-horizontal');
+            }, 1000);
+        }
+    });
+});
